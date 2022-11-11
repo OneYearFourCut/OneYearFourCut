5e6bfd3aece71c2ed2f8e55c724fb88464e245ab
philE22 <71927001+philE22@users.noreply.github.com>
2022-11-11 19:47:16 +0900

    spring 프로젝트 세팅

---
+++ back/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/OneyearfourcutApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.mainproject.oneyearfourcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneyearfourcutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneyearfourcutApplication.class, args);
+	}
+
+}
