b9d324a09cb6d7d462e8bd73da5dac0b1a63643f
Oheun Kim <dhdms8696@naver.com>
2022-11-29 00:05:29 +0900

    ✨FEAT : 전시관 관리 (#110)
    - 저장 시 form 데이터 출력으로 변경
    - useGalleryData 확인 가능

--- client/src/GallerySetting/GallerySetting.tsx
+++ client/src/GallerySetting/GallerySetting.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
 import Input from './components/Input';
+import { jsonInstance } from 'shared/utils/axios';
+import { useGalleryData } from './hooks/useGalleryData';
+
+const postGallery = (form: { title: string; content: string }) => {
+  // return jsonInstance.post(`/galleries/${galleryId}`, form);
+  // return jsonInstance.post(`/galleries`, form);
+  console.log(form);
+};
 
 const GallerySetting = () => {
-  const onSubmit = (form: { name: string; description: string }) => {
-    // TODO: api 통신 작업 후 post로 수정 예정
-    // console.log(form);
+  const onSubmit = (form: { title: string; content: string }) => {
+    postGallery(form);
   };
 
+  // console.log(useGalleryData(1));
   return (
     <div>
       <Input onSubmit={onSubmit} />
