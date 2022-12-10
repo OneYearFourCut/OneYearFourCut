61064f36a1947e224042161d5ecd50879805cf2f
wonkang2 <wonkang96@gmail.com>
2022-12-10 22:25:42 +0900

    [Refactor]Comment entity Artwork 연관 관계 매핑

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/comment/entity/Comment.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/comment/entity/Comment.java
@@ -2,6 +2,7 @@
 
 package com.codestates.mainproject.oneyearfourcut.domain.comment.entity;
 
+import com.codestates.mainproject.oneyearfourcut.domain.artwork.entity.Artwork;
 import com.codestates.mainproject.oneyearfourcut.domain.comment.dto.CommentArtworkResDto;
 import com.codestates.mainproject.oneyearfourcut.domain.comment.dto.CommentGalleryResDto;
 import com.codestates.mainproject.oneyearfourcut.domain.gallery.entity.Gallery;
@@ -33,8 +34,10 @@ public class Comment extends Auditable {
     @JoinColumn(name = "gallery_id")
     private Gallery gallery;  //작품이 아닌 전시관 전체 댓글일때
 
-    @Column
-    private Long artworkId;  //작품에 달린 댓글일때. 비식별관계
+    // Artwork 연관관계 매핑 추가 (단방향)
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "art_id")
+    private Artwork artwork;
 
     @OneToMany(mappedBy = "comment")
     public List<Reply> replyList = new ArrayList<>(); // 대댓글, targetEntity
@@ -42,6 +45,9 @@ public class Comment extends Auditable {
     @Enumerated(EnumType.STRING)
     private CommentStatus commentStatus;
 
+    /* 에러 방지용으로 일단 관련 필드 및 생성자 삭제 안 함 - wk */
+    @Column
+    private Long artworkId;
     @Builder
     public Comment(Long commentId, String content, Member member, Gallery gallery, Long artworkId, CommentStatus commentStatus) {
         this.commentId = commentId;
@@ -51,8 +57,16 @@ public class Comment extends Auditable {
         this.artworkId = artworkId;
         this.commentStatus = commentStatus;
     }
-
-
+    /* 여기까지 리팩토링 완료 후 삭제 예정*/
+    @Builder
+    public Comment(Long commentId, String content, Member member, Gallery gallery, Artwork artwork) {
+        this.commentId = commentId;
+        this.content = content;
+        this.member = member;
+        this.gallery = gallery;
+        this.artwork = artwork;
+        this.commentStatus = CommentStatus.VALID;
+    }
     public void changeContent(String content) {
         this.content = content;
     }
@@ -61,6 +75,7 @@ public class Comment extends Auditable {
         this.commentStatus = commentStatus;
     }
 
+    // 전시관 - 댓글 조회용 (전체)
     public CommentGalleryResDto toCommentGalleryResponseDto(String imagePath){
         return CommentGalleryResDto.builder()
                 .commentId(this.getCommentId())
@@ -73,7 +88,47 @@ public class Comment extends Auditable {
                 .imagePath(imagePath)
                 .build();
     }
+    // 원강 - 수정
+    public CommentGalleryResDto toCommentGalleryResponseDto() {
+        return CommentGalleryResDto.builder()
+                .createdAt(this.createdAt)
+                .modifiedAt(this.modifiedAt)
+                .content(this.content)
+                .artworkId(this.getArtworkId())
+                .memberId(this.member.getMemberId())
+                .nickname(this.member.getNickname())
+                .imagePath(this.getImagePath())
+                .build();
+    }
 
+    public void setGallery(Gallery gallery) {
+        this.gallery = gallery;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setArtwork(Artwork artwork) {
+        this.artwork = artwork;
+    }
+
+    public Long getArtworkId() {
+        if (this.artwork == null) {
+            return null;
+        }
+        return artwork.getArtworkId();
+    }
+
+    public String getImagePath() {
+        if (this.artwork == null) {
+            return null;
+        }
+        return artwork.getImagePath();
+    }
+    /* 여기까지 수정 */
+
+    // 작품 - 댓글 조회용
     public CommentArtworkResDto toCommentArtworkResponseDto(){
         return CommentArtworkResDto.builder()
                 .commentId(this.getCommentId())
