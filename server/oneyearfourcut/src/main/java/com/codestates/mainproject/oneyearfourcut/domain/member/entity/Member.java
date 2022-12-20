4aee248cc59539a531391bca0214434c2848a10b
philE22 <moonwhlking@gmail.com>
2022-12-21 03:50:44 +0900

    D25-6 [Fix] 회원 엔티티 이름 글자수 제한 제거

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/member/entity/Member.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/member/entity/Member.java
@@ -25,7 +25,7 @@ public class Member extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
-    @Column(length = 8, nullable = false)
+    @Column(nullable = false)
     private String nickname;
 
     @Column(nullable = false, unique = true)
