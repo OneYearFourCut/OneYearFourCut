8d92d3a8465cde96c653649fdf566da09aa723a7
DY <86215246+kkdy21@users.noreply.github.com>
2022-12-22 11:45:18 +0900

    Revert "D25-18 ☔️TEST: 테스트 코드"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,8 +17,6 @@ const GalleryAllPic = React.lazy(() => import('Gallery/GalleryAllPic'));
 const SingleComment = React.lazy(() => import('./SingleComments/index'));
 const AllComment = React.lazy(() => import('./AllComments/AllComments'));
 
-console.log('CD test');
-
 const router = createBrowserRouter([
   {
     path: '/',
