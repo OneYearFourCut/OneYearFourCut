30adf10bc03c5fd8d5ab233cbf2c98620e220562
dorrion <dhdms8696@naver.com>
2022-12-26 03:22:37 +0900

    D25-8 ✨FEAT : 로그인 페이지 이동 Alert 구현
    - 취소 시 메인으로 이동

--- client/src/shared/hooks/useAuth.tsx
+++ client/src/shared/hooks/useAuth.tsx
@@ -1,17 +1,25 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { loginStore, historyStore } from 'store/store';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { loginStore, historyStore} from 'store/store';
 
 const AuthCheck = ({ children }: any) => {
   const { isLoggedin } = loginStore();
   const { setHistory } = historyStore();
   const { pathname } = useLocation(); // 지금 위치 기억
   const login_url = process.env.REACT_APP_KAKAO_AUTH_URL;
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!isLoggedin) {
-      setHistory(pathname); // 가야 할 경로
-      window.location.replace(login_url!);
+      let confirmflag = window.confirm(
+        '로그인이 필요한 페이지입니다 로그인하시겠습니까?',
+      );
+      if (confirmflag) {
+        setHistory(pathname); // 가야 할 경로
+        window.location.replace(login_url!);
+      } else {
+        navigate('/');
+      }
     }
   }, []);
   return children;
