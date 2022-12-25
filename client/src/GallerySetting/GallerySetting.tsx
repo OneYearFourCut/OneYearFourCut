00d81e6f6b8c1057de24724c9ba067083ca5605c
dorrion <dhdms8696@naver.com>
2022-12-25 20:55:08 +0900

    D25-8 ✨FEAT : 모달창 구현

--- client/src/GallerySetting/GallerySetting.tsx
+++ client/src/GallerySetting/GallerySetting.tsx
@@ -1,6 +1,8 @@
 import Input from './components/Input';
 import { useState } from 'react';
 import CustomModal from './components/CustomModal';
+import styled from 'styled-components';
+import { rem } from 'polished';
 
 const GallerySetting = () => {
   interface formType {
@@ -14,12 +16,30 @@ const GallerySetting = () => {
     setIsOpen(true);
     setForm(form!);
   };
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div>
       <Input onSubmit={onSubmit} />
-      {isOpen && <CustomModal title={form?.title} content={form?.content} />}
+      {isOpen && (
+        <ModalBackdropBox onClick={onClose}>
+          <CustomModal title={form?.title} content={form?.content} />
+        </ModalBackdropBox>
+      )}
     </div>
   );
 };
 
 export default GallerySetting;
+
+const ModalBackdropBox = styled.div`
+  width: ${rem(428)};
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.1);
+  position: fixed;
+  top: 0;
+  z-index: 51;
+  ${({ theme }) => theme.flex.center}
+`;
