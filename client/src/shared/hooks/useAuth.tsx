91718078fe9be51646ea62699627f464aa4a5f3f
dorrion <dhdms8696@naver.com>
2022-12-26 03:27:57 +0900

    D25-8 ✨FEAT : 로그인 취소해도 기존 사이트로 돌아가도록 변경

--- client/src/shared/hooks/useAuth.tsx
+++ client/src/shared/hooks/useAuth.tsx
@@ -1,13 +1,18 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { loginStore, historyStore} from 'store/store';
+import { useLocation } from 'react-router-dom';
+import { loginStore, historyStore } from 'store/store';
+import { getinitUrl } from 'shared/libs/saveSessionStorage';
+import { useNavigateSearch } from 'shared/hooks/useNavigateSearch';
 
 const AuthCheck = ({ children }: any) => {
   const { isLoggedin } = loginStore();
   const { setHistory } = historyStore();
   const { pathname } = useLocation(); // 지금 위치 기억
   const login_url = process.env.REACT_APP_KAKAO_AUTH_URL;
-  const navigate = useNavigate();
+  const navigateSearch = useNavigateSearch();
+  const handleHeaderTitle = () => {
+    navigateSearch(getinitUrl(), {});
+  };
 
   useEffect(() => {
     if (!isLoggedin) {
@@ -18,7 +23,7 @@ const AuthCheck = ({ children }: any) => {
         setHistory(pathname); // 가야 할 경로
         window.location.replace(login_url!);
       } else {
-        navigate('/');
+        handleHeaderTitle();
       }
     }
   }, []);
