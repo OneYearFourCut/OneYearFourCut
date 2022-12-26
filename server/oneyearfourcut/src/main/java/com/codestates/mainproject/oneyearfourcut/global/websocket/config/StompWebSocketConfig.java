88386fd66043114f2b92277a973d5db653c0463c
wonkang2 <wonkang96@gmail.com>
2022-12-26 19:49:46 +0900

    D25-22 websocket(sockJS),stomp 설정

---
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/global/websocket/config/StompWebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.codestates.mainproject.oneyearfourcut.global.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/stomp") // websocket 접속 endpoint
+                .setAllowedOrigins("*") // 테스트용, 실제 : front분들 요청의 origin으로 변경해야 함.
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}
