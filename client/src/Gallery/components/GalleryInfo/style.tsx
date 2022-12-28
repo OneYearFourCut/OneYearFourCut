cdad56ae904388679d494b1dd4f3720b62a0b1e1
dorrion <dhdms8696@naver.com>
2022-12-28 14:56:39 +0900

    💄DESIGN: 프로필 추가
    - 팔로워, 팔로잉 확인할 수 있도록 프로필 사진 추가

--- client/src/Gallery/components/GalleryInfo/style.tsx
+++ client/src/Gallery/components/GalleryInfo/style.tsx
@@ -2,29 +2,37 @@ import styled from 'styled-components';
 import { rem } from 'polished';
 
 export const Info = styled.div`
-  ${({ theme }) => theme.mixins.flexBox('column', 'center', 'center')}
-  max-width: ${rem(428)};
-  padding: ${rem(10)};
+  ${({ theme }) => theme.mixins.flexBox('row', 'center', 'flex-start')}
+  width: ${rem(428)};
+  padding: ${rem(20)};
 `;
 
 export const ProfileBox = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('column', 'center', 'center')}
+  width: ${rem(120)};
+  margin-left: ${rem(10)};
+`;
+export const ProfileCircle = styled.div`
   width: ${rem(50)};
   height: ${rem(50)};
   border-radius: 50%;
   overflow: hidden;
-  margin-right: ${rem(20)};
 `;
+
 export const Profile = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
 `;
 
+export const InfoBox = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('column', 'flex-start', 'center')}
+`;
+
 export const Title = styled.h2`
-  text-align: center;
+  text-align: left;
 `;
 
 export const Content = styled.div`
-  text-align: center;
-  width: ${rem(428)};
+  text-align: left;
 `;
