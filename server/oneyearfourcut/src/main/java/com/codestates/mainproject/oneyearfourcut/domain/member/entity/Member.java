41d70a242557624569272c5854bec56166c9ac04
philE22 <moonwhlking@gmail.com>
2022-12-29 04:01:32 +0900

    D25-20 [Fix] refresh token 단방향 맵핑으로 변경

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/member/entity/Member.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/domain/member/entity/Member.java
@@ -106,6 +106,6 @@ public class Member extends Auditable {
     @OneToMany(mappedBy = "member")
     private List<Alarm> alarmList = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY, mappedBy = "member")
-    private RefreshToken refreshToken;
+//    @OneToOne(fetch = FetchType.LAZY, mappedBy = "member")
+//    private RefreshToken refreshToken;
 }
