db6bba92fd2751b1844447809f4ceb1ef4893c56
dorrion <dhdms8696@naver.com>
2022-12-29 11:20:49 +0900

    D25-26 ✨FEAT : 채팅방 목록 추가

---
+++ client/src/ChatList/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './style';
+import ChatWrapper from './components/ChatWrapper';
+
+export default function index() {
+  return (
+    <S.Container>
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+      <ChatWrapper />
+    </S.Container>
+  );
+}
