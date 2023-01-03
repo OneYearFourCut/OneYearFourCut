bda1d561303b07d89494b5c593fafe9821c3a098
dorrion <dhdms8696@naver.com>
2023-01-04 03:19:39 +0900

    D25-29 Feat: 채팅방 데이터 map 구현

--- client/src/ChatList/index.tsx
+++ client/src/ChatList/index.tsx
@@ -1,19 +1,10 @@
 import * as S from './style';
 import ChatWrapper from './components/ChatWrapper';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { setStoredToken, getStoredToken } from 'Intro/hooks/tokenStorage';
 import { NativeEventSource, EventSourcePolyfill } from 'event-source-polyfill';
 
 export default function Index() {
-  interface ChatListProps {
-    chatRoomId: number;
-    galleryId: number;
-    profile: string;
-    nickName: string;
-    chattedAt: string;
-    lastChatMessage: string;
-  }
-
   const [status, setStatus] = useState('idle');
   const [chatLists, setChatLists] = useState([]);
   const ACCESS_TOKEN = getStoredToken()?.access_token;
@@ -34,10 +25,17 @@ export default function Index() {
 
     // SSE 열려
     eventSource.onopen = async (event) => {
-      const res = await event.target;
-      console.log(res);
       console.log('connection open');
     };
+
+    // 초반에 채팅 리스트 데이터
+    eventSource.addEventListener(
+      'chatRoom',
+      (e: any) => {
+        setChatLists(e.data);
+      },
+      false,
+    );
 
     // 서버로부터 데이터가 오면
     eventSource.onmessage = async (event) => {
@@ -50,27 +48,85 @@ export default function Index() {
     eventSource.onerror = (event) => {
       // console.log(event.target.readyState);
       console.log(event);
-      // if (event.target.readyState === EventSource.CLOSED) {
-      //   console.log('eventsource closed (' + event.target.readyState + ')');
-      // }
+      if (event.target.readyState === EventSource.CLOSED) {
+        console.log('eventsource closed (' + event.target.readyState + ')');
+      }
     };
     return () => {
       eventSource.close();
     };
   }, []);
 
-  console.log(chatLists);
-  // return <S.Container>{Chatlist}</S.Container>;
-  return <S.Container />;
+  let abc = [
+    {
+      chatRoomId: 7,
+      profile:
+        'http://k.kakaocdn.net/dn/cs8QmZ/btrOQi6QnJZ/kKvGMJYkjA7qsDm8mMd2XK/img_640x640.jpg',
+      nickName: '고대연',
+      galleryId: 59,
+      chattedAt: '2023-01-03T23:51',
+      lastChatMessage: 'test',
+    },
+    {
+      chatRoomId: 7,
+      profile:
+        'http://k.kakaocdn.net/dn/cs8QmZ/btrOQi6QnJZ/kKvGMJYkjA7qsDm8mMd2XK/img_640x640.jpg',
+      nickName: '고대연',
+      galleryId: 59,
+      chattedAt: '2023-01-03T23:51',
+      lastChatMessage: 'test',
+    },
+    {
+      chatRoomId: 7,
+      profile:
+        'http://k.kakaocdn.net/dn/cs8QmZ/btrOQi6QnJZ/kKvGMJYkjA7qsDm8mMd2XK/img_640x640.jpg',
+      nickName: '고대연',
+      galleryId: 59,
+      chattedAt: '2023-01-03T23:51',
+      lastChatMessage: 'test',
+    },
+    {
+      chatRoomId: 7,
+      profile:
+        'http://k.kakaocdn.net/dn/cs8QmZ/btrOQi6QnJZ/kKvGMJYkjA7qsDm8mMd2XK/img_640x640.jpg',
+      nickName: '고대연',
+      galleryId: 59,
+      chattedAt: '2023-01-03T23:51',
+      lastChatMessage: 'test',
+    },
+  ];
+  interface ChatListProps {
+    chatRoomId?: number;
+    galleryId?: number;
+    profile?: string;
+    nickName?: string;
+    chattedAt?: string;
+    lastChatMessage?: string;
+  }
+  const chatList = abc.map(
+    (
+      {
+        chatRoomId,
+        galleryId,
+        profile,
+        nickName,
+        chattedAt,
+        lastChatMessage,
+      }: ChatListProps,
+      idx,
+    ) => {
+      return (
+        <ChatWrapper
+          key={idx}
+          chatRoomId={chatRoomId}
+          galleryId={galleryId}
+          profile={profile}
+          nickName={nickName}
+          chattedAt={chattedAt}
+          lastChatMessage={lastChatMessage}
+        />
+      );
+    },
+  );
+  return <S.Container>{chatList}</S.Container>;
 }
-
-/*
-{
-"chatRoomId": 1,
-"galleryId" : 3,
-"profile": "/porifile.jpg",
-"nickName": "gallery",
-"chattedAt": "2022-12-30T21:19",
-"lastChatMessage": "안녕"
-}
-*/
