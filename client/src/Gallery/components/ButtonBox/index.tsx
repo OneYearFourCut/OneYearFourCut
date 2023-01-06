dfa009bde299260c3ba33be264005e38f6e7e686
dorrion <dhdms8696@naver.com>
2023-01-06 18:05:04 +0900

    D25-33 버튼 스타일 변경
    - 로그인 여부에 따라 보이지 않도록
    - 채팅방 접속 위치 변경

--- client/src/Gallery/components/ButtonBox/index.tsx
+++ client/src/Gallery/components/ButtonBox/index.tsx
@@ -7,6 +7,7 @@ import GalleryType from 'GallerySetting/galleryType';
 import apis from 'Gallery/api';
 import { useGalleryData } from 'GallerySetting/hooks/useGalleryData';
 import { enCryption } from 'shared/libs/cryption';
+import { loginStore } from 'store/store';
 
 const Index = ({ galleryId }: GalleryType) => {
   const navigate = useNavigate();
@@ -14,36 +15,32 @@ const Index = ({ galleryId }: GalleryType) => {
     navigate(`/uploadPicture/${galleryId}`);
   };
   const { data } = useGalleryData(galleryId!);
-
+  const { user } = loginStore();
   return (
     <div>
       <S.BtnContainer>
-        <SmallBtn
-          className='round'
-          onClick={() => {
-            navigate(`/chatlist`);
-          }}
-        >
-          채팅방
-        </SmallBtn>
-        {/* <SmallBtn className='round'>팔로우</SmallBtn> */}
-        <S.smBtn
-          className='ml-8'
-          onClick={() => {
-            apis.createChat(data.memberId).then((res) => {
-              navigate(`/chatroom/${enCryption(res.data.chatRoomId)}`);
-            });
-          }}
-        >
-          <Chat />
-        </S.smBtn>
-        <IconBtn
-          onClick={handleClick}
-          className='white ml-56'
-          icon={<Camera />}
-        >
-          <p>사진 올려주기</p>
-        </IconBtn>
+        {/* <SmallBtn className='round disabled'>팔로우</SmallBtn> */}
+        {galleryId !== user?.galleryId && (
+          <IconBtn
+            className='ml-8 smallIcon'
+            onClick={() => {
+              apis.createChat(data.memberId).then((res) => {
+                navigate(`/chatroom/${enCryption(res.data.chatRoomId)}`);
+              });
+            }}
+          >
+            DM <Chat />
+          </IconBtn>
+        )}
+        {galleryId !== user?.galleryId && (
+          <IconBtn
+            onClick={handleClick}
+            className='white  ml-32'
+            icon={<Camera />}
+          >
+            <p>사진 올려주기</p>
+          </IconBtn>
+        )}
       </S.BtnContainer>
     </div>
   );
