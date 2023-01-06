df7a2443d10dc31400952213778725895227137c
philE22 <moonwhlking@gmail.com>
2023-01-06 16:20:23 +0900

    D25-22 [Fix] AOP 추가, sse 타임아웃 30초 변경

--- server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/global/log/LogAop.java
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/global/log/LogAop.java
@@ -1,7 +1,10 @@
 package com.codestates.mainproject.oneyearfourcut.global.log;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -9,5 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAop {
 
+    @Pointcut("execution(* com.codestates.mainproject.oneyearfourcut.domain.*.controller..*(..))")
+    private void allController() {}
 
+    @Around("allController()")
+    public Object galleryLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("#################################################");
+        log.info("#########{} -> {}##########", joinPoint.getSignature().getDeclaringType().getSimpleName(), joinPoint.getSignature().getName());
+        log.info("#################################################");
+
+        return joinPoint.proceed();
+    }
 }
