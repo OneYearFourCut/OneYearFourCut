676190255f721e0c3d85ee02f4f28d3a9ae4ceab
dorrion <dhdms8696@naver.com>
2023-01-06 15:28:53 +0900

    D25-33 456 457 에러 외에 에러 해결 코드

--- client/src/ChatList/index.tsx
+++ client/src/ChatList/index.tsx
@@ -1,6 +1,6 @@
 import * as S from './style';
 import ChatWrapper from './components/ChatWrapper';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { setStoredToken, getStoredToken } from 'Intro/hooks/tokenStorage';
 import { NativeEventSource, EventSourcePolyfill } from 'event-source-polyfill';
 import apis from 'shared/components/Header/api';
@@ -15,7 +15,7 @@ export default function Index() {
     lastChatMessage?: string;
   }
   const [chatLists, setChatLists] = useState<Array<ChatListProps>>([]);
-  const [connecting, setConnecting] = useState<boolean>(false);
+  const [reConnectCount, setReConnectCount] = useState<number>(0);
 
   const EventSource = EventSourcePolyfill || NativeEventSource;
   let eventSource: any = null;
@@ -36,18 +36,25 @@ export default function Index() {
     eventSource.onopen = async (e: any) => {
       console.log('connection open');
     };
+
+    // 초반에 채팅 리스트 데이터
+    eventSource.addEventListener(
+      'chatRoom',
+      (e: any) => {
+        // 채팅방 목록 데이터 저장
+        console.log(JSON.parse(e.data));
+        setChatLists(JSON.parse(e.data));
+      },
+      false,
+    );
   }
 
-  const ErrorHandler = () => {
-    apis.getRefreshedToken().then(() => {
-      eventSource.close();
-      Connect();
-    });
-  };
-
   const Change = (data: any) => {
+    console.log('얘는 새로 온 데이터 : ', data);
+    console.log('얘는 기존 리스트 :', chatLists);
     return chatLists.map((el: any) => {
       // 메세지로 들어올 채팅방 번호 같으면
+      console.log(el);
       if (el.chatRoomId === data.chatRoomId) {
         el.chattedAt = data.chattedAt;
         el.lastChatMessage = data.lastChatMessage;
@@ -58,26 +65,15 @@ export default function Index() {
   };
 
   const EventHandler = () => {
-
-    // 초반에 채팅 리스트 데이터
-    eventSource.addEventListener(
-      'chatRoom',
-      (e: any) => {
-        // 채팅방 목록 데이터 저장
-        setChatLists(JSON.parse(e.data));
-      },
-      false,
-    );
-
     eventSource.addEventListener('message', (e: any) => {
       let data = JSON.parse(e.data);
+      console.log(Change(data));
       setChatLists(Change(data));
     });
 
     eventSource.addEventListener('error', (err: any) => {
       console.log('에러 발생: ', err.status);
       if (err.status === 456) {
-        // ErrorHandler();
         apis
           .getRefreshedToken()
           .then(() => {
@@ -88,6 +84,15 @@ export default function Index() {
       } else if (err.status === 457) {
         alert('로그인이 만료되었습니다.');
         window.location.replace('/');
+      } else {
+        if (reConnectCount < 3) {
+          setReConnectCount(reConnectCount + 1);
+          eventSource.close();
+          Connect();
+        } else {
+          alert('eventSource server error');
+          window.location.replace('/');
+        }
       }
     });
   };
@@ -102,6 +107,8 @@ export default function Index() {
       console.log('eventsource closed');
     };
   }, []);
+
+  console.log(chatLists);
 
   const chatList = chatLists.map(
     (
