90a8e4c21eb1e816f3a1ac991a595250859091ac
philE22 <moonwhlking@gmail.com>
2023-01-06 15:03:58 +0900

    D25-22 [Fix] sql trace 끔

---
+++ server/oneyearfourcut/src/main/java/com/codestates/mainproject/oneyearfourcut/global/log/LogAop.java
@@ -0,0 +1,13 @@
+package com.codestates.mainproject.oneyearfourcut.global.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAop {
+
+
+}
